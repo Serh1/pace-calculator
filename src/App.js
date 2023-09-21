7ad1f3c230c020d4fbac5b9eb9dfbbcae31cc960
Mateiu <s.mateiu@yahoo.com>
2023-09-21 17:58:46 +0300

    Responsive and layouts

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import Input from "./components/Input";
 import background from "./assets/img/background.png";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Pacer from "./components/Pacer/Pacer";
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto&family=Viga&display=swap');
-</style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto&family=Viga&display=swap');
+</style>;
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <Pacer/>
+      <Header />
     </>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,23 @@
-import React from 'react'
-import "./Header.css"
-import Navbar from '../Navbar/Navbar'
-import EarthBanner from '../../assets/img/earth.png'
+import React from "react";
+import "./Header.css";
+import Navbar from "../Navbar/Navbar";
+import EarthBanner from "../../assets/img/earth.png";
+import Input from "../Input";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="wrapper">
-                <Navbar/>
-                <div className="cta">
-                    <div className='title-secondary'>YOUR</div>
-                    <div className='title-main'> PACE </div>
-                    <p className='description'>
-                        "Revolutionize your visual content with our cutting-edge AI image generation technology. Enhance your brand with unique and captivating visuals, made easy with our AI platform."
-                    </p>
-                    <div className="list-wrapper">
-                        <ul>
-                            <li>Take A Look</li>
-                            <li>3D Art</li>
-                            <li>AI Image Generation</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <img className='earth_img' src={EarthBanner}></img>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div className="wrapper">
+        <Navbar />
+        <div className="content-wrapper">
+          <div className="title-secondary">YOUR</div>
+          <div className="title-main"> PACE </div>
+          <Input />
+        </div>
+      </div>
+      <img className="earth_img" src={EarthBanner}></img>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
